c806607f98d260362c3ad7573ed119380e4504e2
Dzenan Jupic <info@dzenanjupic.de>
2020-02-26 20:20:27 +0100

    Startet writing some code
    Current state: can output some text at boot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![allow(dead_code)]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+
+#[no_mangle] // makes function name not change
+extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
+
+
+
+#[panic_handler] // gets invoked if program panics (should never happen)
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
